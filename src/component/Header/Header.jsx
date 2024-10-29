5d3b8b03b0d5f2069f7ec6882037b57ed7829d06
lwambon <lwambonyaga@gmail.com>
2024-10-29 22:23:45 +0300

    formatting the header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,11 @@
 import "./Header.css";
+import Logo from "../../assets/nairobilogo2.jpg";
 
 function Header() {
   return (
     <div className="header-section">
       <div className="header-navigation">
-        <h2 className="header-logo">online car dealering</h2>
+        <img src={Logo} alt="logo" className="header-logo" />
         <div className="navigation-list">
           <nav className="navigation-items">
             <a href="">home</a>
